ae1a5fef03ee12db5b1a1b8efc99327ef2f4e2d1
Ingo Breßler <dev@ingobressler.net>
2013-03-25 16:16:29 +0100

    append __init__ docstring to class docstring

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -111,6 +111,8 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+# append __init__(self) doctring to class docstring
+autoclass_content = 'both'
 
 # -- Options for HTML output ---------------------------------------------------
 
