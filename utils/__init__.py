ef0a4ada31f7649e858efe1a1469f1a451dd4876
Ingo Breßler <dev@ingobressler.net>
2013-04-23 19:13:56 +0200

    added DataSet class and some utilities

---
+++ utils/__init__.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# __init__.py
+
+from sys import float_info
+import collections
+import platform
+import hashlib
+import os.path
+import logging
+
+QString = basestring
+EPS = float_info.epsilon
+
+# object tests
+
+def isList(obj):
+    return not isString(obj) and isinstance(obj, collections.Sequence)
+
+def isString(obj):
+    return isinstance(obj, basestring) or isinstance(obj, QString)
+
+def isNonEmptyString(obj):
+    return (isString(obj) and len(obj) > 0)
+
+def isMap(obj):
+    return isinstance(obj, collections.Mapping)
+
+def isSet(obj):
+    return isinstance(obj, collections.Set)
+
+def isNumber(obj):
+    return (isinstance(obj, int) or isinstance(obj, float) or
+            isinstance(obj, long) or isinstance(obj, complex))
+
+def isInteger(obj):
+    return (isinstance(obj, int) or isinstance(obj, long))
+
+# environment tests
+
+def isLinux():
+    return platform.system().lower() in "linux"
+
+def isWindows():
+    return platform.system().lower() in "windows"
+
+def isFrozen():
+    return hasattr(sys, "frozen")
+
+# utility functions
+
+def makeHash(s):
+    m = hashlib.sha1()
+    m.update(s)
+    return m.hexdigest()
+
+def makeFilename(path, basename, suffix, ext):
+    """Creates a file name based on provided information."""
+    return os.path.join(path, basename +'_'+ suffix +'.'+ ext)
+
+def formatList(lst, precision = 4):
+    fmt = ":." + str(precision) + "f"
+    fmt = "; ".join(["{" + str(i) + fmt + "}"
+        for i in range(0, len(lst))])
+    return fmt.format(*lst)
+
+#import inspect
+
+def getTypeFromModules(name, modNames):
+#    print "searching for", name
+#    for i in range(0, 4):
+#        print i
+#        frame = inspect.stack()[i]
+#        print "TEST", frame[0].f_globals.get('__name__', None)
+    if not isList(modNames):
+        modNames = (modNames, )
+    objType = None
+    for modName in modNames:
+        try:
+            mod = __import__(modName)
+        except StandardError:
+            continue
+        objType = getattr(mod, name, None)
+        if objType is not None:
+            break
+    assert objType is not None, ("Object type {0} not found in modules {1}!"
+                                 .format(name, modNames))
+    return objType
+
+def transformToString(t):
+    """
+    Generates a printable string in matrix form of a QTransform.
+
+    >>> from utilsgui import printQTransform
+    >>> printQTransform(None)
+    ''
+    >>> from PyQt4.QtGui import QTransform
+    >>> printQTransform(  QTransform.fromScale(1.2, 0.7)
+    ...                 * QTransform.fromTranslate(0.3, 1.4))
+    '\\n+1.200e+00 +0.000e+00 +0.000e+00 \\n+0.000e+00 +7.000e-01 +0.000e+00 \\n+3.000e-01 +1.400e+00 +1.000e+00 '
+    """
+    res = ""
+    if t is None:
+        return res
+    for row in [[t.m11(), t.m12(), t.m13()], \
+                [t.m21(), t.m22(), t.m23()], \
+                [t.m31(), t.m32(), t.m33()]]:
+        res += "\n"
+        for col in row:
+            res += "{0:>+10.3e} ".format(col)
+    return res
+
+# vim: set ts=4 sw=4 sts=4 tw=0:
