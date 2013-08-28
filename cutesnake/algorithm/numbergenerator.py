cb5ab058b90934d63f48ed1f3a0a776427d51c99
Ingo Breßler <dev@ingobressler.net>
2013-08-28 18:22:56 +0200

    common base for RandomExponential variants

--- cutesnake/algorithm/numbergenerator.py
+++ cutesnake/algorithm/numbergenerator.py
@@ -21,43 +21,26 @@ class RandomUniform(NumberGenerator):
         return numpy.random.uniform(size = count)
 
 class RandomExponential(NumberGenerator):
+    lower, upper = 0., 1.
+
     @classmethod
     def get(cls, count = 1):
-        # numpy.random.exponential is an unbound function, may cause issues
-        # as it may assume values >1. Therefore, a new sample is drawn upon
-        # its occurrence
-        rs = numpy.random.exponential(size = count)
-        while rs.max() > 1.:
-            rs[rs > 1.] = numpy.random.exponential(size = (rs > 1.).sum())
+        rs = 10**(numpy.random.uniform(cls.lower, cls.upper, count))
+        rs = (rs - 1) / (10**(cls.upper - cls.lower))
         return rs
 
-class RandomExponential1(NumberGenerator):
-    """ picks values with inverse logarithmic probability, as if it were 
-    spanning one decade """
+class RandomExponential1(RandomExponential):
+    """Alias class for RandomExponential"""
+    pass
 
-    @classmethod
-    def get(cls, count = 1):
-        Rs=10**(numpy.random.uniform(0.,1.,count))
-        Rs=(Rs-1)/9.
-        return Rs
+class RandomExponential2(RandomExponential):
+    """Picks values with inverse logarithmic probability over )0, 1(
+    , as if it were spanning two decades."""
+    upper = 2.
 
-class RandomExponential2(NumberGenerator):
-    """ picks values with inverse logarithmic probability, as if it were 
-    spanning two decades """
+class RandomExponential3(RandomExponential):
+    """Picks values with inverse logarithmic probability over )0, 1(
+    , as if it were spanning three decades."""
+    upper = 3.
 
-    @classmethod
-    def get(cls, count = 1):
-        Rs=10**(numpy.random.uniform(0.,2.,count))
-        Rs=(Rs-1)/99.
-        return Rs
-
-class RandomExponential3(NumberGenerator):
-    """ picks values with inverse logarithmic probability, as if it were 
-    spanning three decades """
-
-    @classmethod
-    def get(cls, count = 1):
-        Rs=10**(numpy.random.uniform(0.,3.,count))
-        Rs=(Rs-1)/999.
-        return Rs
 # vim: set ts=4 sts=4 sw=4 tw=0:
