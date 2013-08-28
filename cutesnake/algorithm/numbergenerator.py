f7c5b6cc23d720d186e208a719b3276503c17d6b
Brian Richard Pauw <brian@stack.nl>
2013-08-28 17:25:35 +0900

    Added three new random number generators, sampling logarithmically as if the sampling range were spanning one, two or three decades.

--- cutesnake/algorithm/numbergenerator.py
+++ cutesnake/algorithm/numbergenerator.py
@@ -23,16 +23,41 @@ class RandomUniform(NumberGenerator):
 class RandomExponential(NumberGenerator):
     @classmethod
     def get(cls, count = 1):
-        #numpy.random.exponential is an unbound function, may cause issues
-        #as it may assume values >1. Therefore, a new sample is drawn upon its
-        #occurrence
-        Rs=numpy.random.exponential(size = count)
-        if count>1:
-            while Rs.max()>1.:
-                Rs[Rs>1.]=numpy.random.exponential(size=(Rs>1.).sum())
-        else:
-            while Rs>1.:
-                Rs=numpy.random.exponential()
+        # numpy.random.exponential is an unbound function, may cause issues
+        # as it may assume values >1. Therefore, a new sample is drawn upon
+        # its occurrence
+        rs = numpy.random.exponential(size = count)
+        while rs.max() > 1.:
+            rs[rs > 1.] = numpy.random.exponential(size = (rs > 1.).sum())
+        return rs
+
+class RandomExponential1(NumberGenerator):
+    """ picks values with inverse logarithmic probability, as if it were 
+    spanning one decade """
+
+    @classmethod
+    def get(cls, count = 1):
+        Rs=10**(numpy.random.uniform(0.,1.,count))
+        Rs=(Rs-1)/9.
         return Rs
 
+class RandomExponential2(NumberGenerator):
+    """ picks values with inverse logarithmic probability, as if it were 
+    spanning two decades """
+
+    @classmethod
+    def get(cls, count = 1):
+        Rs=10**(numpy.random.uniform(0.,2.,count))
+        Rs=(Rs-1)/99.
+        return Rs
+
+class RandomExponential3(NumberGenerator):
+    """ picks values with inverse logarithmic probability, as if it were 
+    spanning three decades """
+
+    @classmethod
+    def get(cls, count = 1):
+        Rs=10**(numpy.random.uniform(0.,3.,count))
+        Rs=(Rs-1)/999.
+        return Rs
 # vim: set ts=4 sts=4 sw=4 tw=0:
