5fa52bbd44802716cae861488d24e9a2567824fe
Ingo Breßler <dev@ingobressler.net>
2013-08-30 19:47:21 +0200

    reviewed log handler setup and redirection; Sink classes in separate file

--- cutesnake/log/__init__.py
+++ cutesnake/log/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 # __init__.py
 
-from log import Log
+import logging
+from log import (timestampFormat, timestamp, replaceStdOutErr,
+                 replaceHandler, addHandler, removeHandler)
 from widgethandler import WidgetHandler
+
+log = logging.getLogger()
 
 # vim: set ts=4 sw=4 sts=4 tw=0:
