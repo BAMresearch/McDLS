d80ef2dd7f9baea7ba3e438523996ec2cc22e6e8
Brian Richard Pauw <brian@stack.nl>
2013-11-06 15:53:46 +0900

    Updated documentation of the McSASParameters class in McSAS.py

--- McSAS.py
+++ McSAS.py
@@ -48,22 +48,7 @@ Example Usage
 -------------
 
 *For detailed usage, please see the* :doc:`quickstart`
-
-Fitting a single Dataset using all automatic and default Parameters
-(may go wrong on poorly conditioned input, needs sensibly-spaced datapoints
-and good uncertainty estimates).
-The Dataset is considered to consist of three variables *Q*, *I* and *IError*::
-
- McSAS(Q = Q, I = I, IError = IError, plot = True)
-
-Optional Parameters can be supplied in parameter-value pairs to finetune
-optimisation behaviour::
-
- A = McSAS(Q = Q, I = I, IError = numpy.maximum(0.01 * I, E),
-           numContribs = 200, convergenceCriterion = 1,
-           contribParamBounds = array([0.5e-9, 35e-9]),
-           maxIterations = 1e5, histogramXScale = 'log',
-           deltaRhoSquared = 1e30, numReps = 100, plot = True)
+*this section will be updated shortly
 
 Module Documentation
 ====================
@@ -92,6 +77,46 @@ from models.sphere import Sphere
 from cutesnake.utilsgui import processEventLoop
 
 class McSASParameters(PropertyNames):
+    """Defines the parameters used for the fitting procedure:
+        - *model*: an instance of McSAS.model defining the fitting model
+        - *contribParamBounds*: Bounds of the active (fitting) parameter
+        - *qBounds*: limits in *q* between which the fit is applied
+        - *psiBounds*: limits in azimuthal angle (2D patterns only) to which
+            the fit is applied
+        - *priors*: a set of contribution parameters for a number of McSAS 
+            repetitions. These can be used to resume or recalculate an older
+            result
+        - *prior*: same as *priors*, but only for a single repetition. Can be
+            used as an initial guess. WARNING: intial guesses may skew result
+        - *histogramBins*: number of bins to use for size distribution
+            determination. Does not affect fit
+        - *histogramXScale*: can be "log" or "linear", sets the horizontal
+            axis scaling. Does not affect fit
+        - *histogramWeighting*: can be "volume"(recommended) or "number". 
+            Determines whether to plot the volume-weighted size distribution
+            (which is closest to what is measured in a scattering measurement)
+            or a number-weighted size distribution.
+        - *deltaRhoSquared* the value (in m^{-4}) of the squared electron 
+            density contrast. Typically on the order of 10^{25} to 10^{30} for
+            X-ray scattering measurements. To get a correct volume fraction, 
+            the intensity must be in reciprocal meters, as must *q* and the
+            object size parameters must be adjusted accordingly.
+        - *startFromMinimum*: may be depreciated: starts with an initial guess
+            consisting of minimum size values rather than random. For testing
+            purposes only.
+        - *maxRetries*: if a single optimisation fails, it will be retried 
+            this integer of times.
+        - *maskNegativeInt*: may be depreciated due to overlap with similar 
+            functionality in the McSAS.data molule. Setting this will ignore 
+            datapoints with intensity values <0
+        - *maskZeroInt*: similar to above, but for intensity values =0
+        - *lowMemoryFootprint*: can be used for models which require a lot 
+            of memory to calculate. Slows down fitting by about 30%, but 
+            reduces the memory requirements drastically as only a single 
+            contribution is calculated at a time
+        - *doPlot*: sets whether to automatically plot or not
+
+    """
     model = None
     contribParamBounds = ()
     qBounds = None
