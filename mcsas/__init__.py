05b8a932fe5911d78f714f5d88fca75741510c2d
Ingo Breßler <dev@ingobressler.net>
2014-01-28 18:56:07 +0100

    removed obsolete code

--- mcsas/__init__.py
+++ mcsas/__init__.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
+# mcsas/__init__.py
+
 """
 McSAS Core
 """
 
-#import numpy # For arrays
-#from numpy import (inf, array, isfinite, reshape, prod, shape, pi, diff, zeros,
-#                  arange, size, sin, cos, sum, sqrt, log10,
-#                  isnan, ndim, newaxis)
-#from scipy import optimize
-#import os # Miscellaneous operating system interfaces
-#import time # Timekeeping and timing of objects
-#import sys # For printing of slightly more advanced messages to stdout
-#from abc import ABCMeta, abstractmethod
-#import inspect
-#
-#from cutesnake.dataset import DataSet
-#from cutesnake.utils import isList, isString, isFrozen
-#from cutesnake.algorithm import (AlgorithmBase, Parameter, ParameterBase, ParameterFloat,
-#                                 RandomUniform, RandomExponential)
-#from utils.propertynames import PropertyNames
-#from models.scatteringmodel import ScatteringModel
-#from models.sphere import Sphere
-#from cutesnake.utilsgui import processEventLoop
-
 pass
+
 # vim: set ts=4 sts=4 sw=4 tw=0:
