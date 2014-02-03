a8959439c6d25867b43cb73155d8162213547964
Brian Richard Pauw <brian@stack.nl>
2014-02-03 15:57:58 +0900

    Updated cylinder model now includes distribution suggestions

--- models/cylindersIsotropic.py
+++ models/cylindersIsotropic.py
@@ -5,6 +5,7 @@ import numpy, scipy, scipy.special
 from numpy import pi, zeros, sin, cos, sqrt, newaxis, sinc
 from utils.parameter import Parameter
 from scatteringmodel import ScatteringModel
+from cutesnake.algorithm import RandomExponential, RandomUniform
 
 # parameters must not be inf
 
@@ -16,12 +17,15 @@ class CylindersIsotropic(ScatteringModel):
     parameters = (
             Parameter("radius", 1.0,
                     displayName = "Cylinder radius",
+                    generator = RandomExponential,
                     valueRange = (0.1, numpy.inf), suffix = "nm"),
             Parameter("length", 10.0,
                     displayName = "length L of the cylinder",
+                    generator = RandomExponential,
                     valueRange = (0.1, numpy.inf), suffix = "nm"),
             Parameter("psiAngle", 0.0,
                     displayName = "internal parameter -- ignore",
+                    generator = RandomUniform,
                     valueRange = (0.01, 180.01), suffix = "deg."),
             Parameter("psiAngleDivisions", 303.,
                     displayName = "orientation integration divisions",
