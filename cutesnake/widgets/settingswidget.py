ec06b50c7d96cc5d2ab59b389e770328a2e7c7f8
Ingo Breßler <dev@ingobressler.net>
2014-05-03 15:54:20 +0200

    dropped PyQt compat for cutesnake.SettingsWidget, signals updates on single widget level

--- cutesnake/widgets/settingswidget.py
+++ cutesnake/widgets/settingswidget.py
@@ -2,15 +2,9 @@
 # settingswidget.py
 
 from cutesnake.qt import QtCore, QtGui
-try: # remove if switching to PySide exclusively
-    from QtCore import QVariant
-except ImportError:
-    class QVariant: # a dummy class for compatibility
-        Int, Double, Bool, String = None, None, None, None
-        toInt, toDouble, toBool, toString = None, None, None, None
-
-from QtGui import QWidget, QSpinBox, QDoubleSpinBox, QLineEdit, QCheckBox
-from QtCore import QSignalMapper
+from QtGui import (QWidget, QSpinBox, QDoubleSpinBox, QLineEdit, QCheckBox,
+                   QAbstractButton, QAbstractSpinBox, QLineEdit)
+from QtCore import QSignalMapper, QObject
 from cutesnake.widgets.expdoublespinbox import ExpDoubleSpinBox
 from cutesnake.utils.signal import Signal
 from cutesnake.utils import isList
@@ -21,8 +15,8 @@ class SettingsWidget(QWidget):
 
     Call get('<objectname>') to get an input widget value.
     """
-    sigValuesChanged = Signal()   # signals a change of any child widget
-    sigValueChanged = Signal(str) # signals a change of a specific widget
+    sigValuesChanged = Signal()       # signals a change of any child widget
+    sigValueChanged = Signal(QWidget) # signals a change of a specific widget
     _signalMapper = None
     _keys = None                  # objectNames of connected input widgets
     _isUpdateRequired = None
@@ -32,16 +26,11 @@ class SettingsWidget(QWidget):
                      str:   QLineEdit,
                      bool:  QCheckBox,
                    }
-    _convertData = { QVariant.Int:    QVariant.toInt,
-                     QVariant.Double: QVariant.toDouble,
-                     QVariant.Bool:   QVariant.toBool,
-                     QVariant.String:
-                        (lambda variant: str(variant.toString())) }
 
     def __init__(self, parent = None):
         QWidget.__init__(self, parent)
         self._signalMapper = QSignalMapper()
-        self._signalMapper.mapped[str].connect(self._editingFinishedSlot)
+        self._signalMapper.mapped[QObject].connect(self._editingFinishedSlot)
         self._keys = set()
         try:
             self.setupUi(self)
@@ -59,54 +48,41 @@ class SettingsWidget(QWidget):
 
     def get(self, key, defaultValue = None):
         child = self.findChild(QWidget, key)
-        if child == None:
-            return defaultValue
-        variant = None
-        for key in "value", "checked", "text":
-            variant = child.property(key)
-            try:
-                if variant.isValid():
-                    break
-            except AttributeError: # QVariant
-                if variant is not None:
-                    break
-        if variant is None:
-            return defaultValue
-        if isinstance(variant, QVariant):
-            if not variant.isValid():
-                return defaultValue
-            variant = self._convertData[variant.type()](variant)
-        value = variant
-        if value is None:
-            return defaultValue
-        # QVariant.toInt, QVariant.toDouble return tuple (value, ok)
-        if isList(value) and len(value) == 2 and isinstance(value[1], bool):
-            if not value[1]:
-                return defaultValue
-            value = value[0]
+        value = defaultValue
+        if child is None:
+            return value
+        propValue = None
+        for attr in "value", "checked", "text":
+            propValue = child.property(attr)
+            if propValue is not None:
+                value = propValue
+                break
         return value
 
     def set(self, key, value):
-        if (value is None or
-            (hasattr(value, "isValid") and not value.isValid())):
-            return
         child = self.findChild(QWidget, key)
-        for key in ("checked", "value", "text"):
-            variant = child.property(key)
+        if child is None:
+            return
+        setter, dtype = None, None
+        for attr in "checked", "value", "text":
+            setterName = "set" + attr.title()
             try:
-                if variant.isValid():
-                    break
-            except AttributeError: # QVariant
-                if variant is not None:
+                setter = getattr(child, setterName)
+                oldValue = getattr(child, attr)()
+                dtype = type(oldValue)
+            except AttributeError:
+                continue
+            else:
+                if dtype is not None:
                     break
         try:
-            success = child.setProperty(key, value)
-            if not success:
-                raise IndexError
-        except StandardError:
+            setter(dtype(value))
+        except TypeError:
             raise IndexError("Could not set widget '{0}' to '{1}'!".
                              format(key, value))
-        child.update()
+        else:
+            self.getEditingFinishedSignal(child).emit()
+            child.update()
 
     def _connect(self, widget):
         self._connectInputWidget(widget)
@@ -116,34 +92,56 @@ class SettingsWidget(QWidget):
     def _connectInputWidget(self, widget):
         def isInputWidget(widget):
             return any([isinstance(widget, widgetType)
-                        for widgetType in self._inputWidget.values()])
+                        for widgetType in QAbstractButton,
+                                          QAbstractSpinBox,
+                                          QLineEdit
+                        ])
         if not isInputWidget(widget):
             return
-        self._signalMapper.setMapping(widget, widget.objectName())
-        if isinstance(widget, QCheckBox):
-            widget.clicked.connect(self._signalMapper.map)
+        self._signalMapper.setMapping(widget, widget)
+        # connect appropriate signals, depends on widget type
+        self.getEditingFinishedSignal(widget).connect(self._signalMapper.map)
+        # value changed signals
+        try: # QAbstractButton
+            widget.clicked[bool].connect(self._valueChangedSlot)
+        except AttributeError: pass
+        try: # QCheckbox
             widget.stateChanged.connect(self._valueChangedSlot)
-        else:
-            widget.editingFinished.connect(self._signalMapper.map)
-            if isinstance(widget, QLineEdit): # no valueChanged signal
-                widget.textChanged.connect(self._valueChangedSlot)
-            else:
-                widget.valueChanged.connect(self._valueChangedSlot)
+        except AttributeError: pass
+        try: # QLineEdit
+            widget.textChanged.connect(self._valueChangedSlot)
+        except AttributeError: pass
+        try: # QSpinbox
+            widget.valueChanged.connect(self._valueChangedSlot)
+        except AttributeError: pass
         # remember input widget keys/names
         self._keys.add(widget.objectName())
 
     connectInputWidgets = _connectInputWidget
 
-    def _editingFinishedSlot(self, key):
+    @staticmethod
+    def getEditingFinishedSignal(widget):
+        if not isinstance(widget, QWidget):
+            return
+        sig = None
+        for sigName in "editingFinished", "clicked":
+            sig = getattr(widget, sigName, None)
+            if sig is not None:
+                break
+        return sig
+
+    def _editingFinishedSlot(self, widget):
         """Called after input widget looses focus,
         usually only one at a time."""
-        if not self._isUpdateRequired:
-            return
+        print "_editingFinishedSlot", self._isUpdateRequired
+        #if not self._isUpdateRequired:
+        #    return
         self._isUpdateRequired = False
         self.sigValuesChanged.emit()
-        self.sigValueChanged.emit(key)
+        self.sigValueChanged.emit(widget)
 
     def _valueChangedSlot(self, dummy):
+        print "_valueChangedSlot", dummy
         self._isUpdateRequired = True
 
 # vim: set sts=4 ts=4 sw=4 tw=0:
