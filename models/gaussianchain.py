25ed86c688fdac31e9904def528cf0efaf899710
Ingo Breßler <dev@ingobressler.net>
2014-05-13 17:45:10 +0200

    GaussianChain: using values directly from model for evaluation, improved testing against reference data

--- models/gaussianchain.py
+++ models/gaussianchain.py
@@ -2,11 +2,10 @@
 # models/gaussianchain.py
 
 import numpy
+from utils import mixedmethod
 from utils.parameter import FitParameter, Parameter
-from scatteringmodel import ScatteringModel
+from models import ScatteringModel
 from cutesnake.algorithm import RandomUniform, RandomExponential
-
-# parameters must not be inf
 
 class GaussianChain(ScatteringModel):
     r"""Form factor of flexible polymer chains which are not selfavoiding
@@ -50,70 +49,33 @@ class GaussianChain(ScatteringModel):
         self.etas.setValueRange((0.1, 10.))
         self.k.setValueRange((0.1, 10.))
 
-    def formfactor(self, dataset, paramValues = None):
-        # vectorized data and arguments
-        q = dataset.q
-        rg = numpy.array((self.rg(),))
-        if self.rg.isActive():
-            rg = paramValues[:, 0]
-        bp = numpy.array((self.bp(),))
-        if self.bp.isActive():
-            idx = int(self.rg.isActive())
-            bp = paramValues[:, idx]
-        etas = numpy.array((self.etas(),))
-        if self.etas.isActive():
-            idx = int(self.rg.isActive()) + int(self.bp.isActive())
-            etas = paramValues[:, idx]
-        k = numpy.array((self.k(),))
-        if self.k.isActive():
-            idx = int(self.rg.isActive()) + int(self.bp.isActive()) + int(self.etas.isActive())
-            k = paramValues[:, idx]
-
-        beta = bp - (k * rg**2) * etas
-        u = numpy.outer(q, rg)**2 # a matrix usually
+    def formfactor(self, dataset, paramValues):
+        # vectorized data
+        beta = self.bp() - (self.k() * self.rg()**2) * self.etas()
+        u = (dataset.q * self.rg())**2
         result = numpy.sqrt(2.) * numpy.sqrt(numpy.expm1(-u) + u) / u
-        for i, res in zip(range(0, result.shape[0]), result):
-            if q[i] <= 0.0:
-                result[i] = beta
-            result[i] = res * beta
+        result *= beta
+        result[dataset.q <= 0.0] = beta
         return result
 
     def volume(self, paramValues):
-        rg = numpy.array((self.rg(),))
-        if self.rg.isActive():
-            rg = paramValues[:, 0]
-        k = numpy.array((self.k(),))
-        if self.k.isActive():
-            idx = sum([int(p.isActive()) for p in self.rg, self.bp, self.etas])
-            k = paramValues[:, idx]
-        v = k * rg**2
-        if len(v) <= 1 and len(v) < len(paramValues):
-            # duplicates of single value for dimension compatibility
-            v = numpy.ones(len(paramValues)) * v
+        v = self.k() * self.rg()**2
         return v**self.compensationExponent
+
+    @mixedmethod
+    def fixTestParams(self, params):
+        # order and meaning differs
+        vol = params['etas']
+        params['etas'] = params['k']
+        params['k'] = vol / params['rg']**2.
+        return params
 
 GaussianChain.factory()
 
 if __name__ == "__main__":
-    from cutesnake.datafile import PDHFile, AsciiFile
-    # FIXME: use SASData.load() instead
-    pf = PDHFile("../brianpauw/sasfit_gauss2-5-1.5-2-1.dat")
-    model = GaussianChain()
-    model.rg.setValue(5.)
-    model.rg.setActive(False)
-    model.bp.setValue(1.5)
-    model.bp.setActive(False)
-    model.etas.setValue(1.)
-    model.etas.setActive(False)
-    model.k.setValue(0.08)
-    model.k.setActive(False)
-    intensity = model.formfactor(pf.data, None).reshape(-1)**2.
-    q = pf.data[:, 0]
-    oldInt = pf.data[:, 1]
-    delta = abs(oldInt - intensity)
-    result = numpy.dstack((q, intensity, delta))[0]
-    AsciiFile.writeFile("gauss.dat", result)
-    # call it like this:
-    # PYTHONPATH=..:../mcsas/ python brianpauwgui/gaussianchain.py && gnuplot -p -e 'set logscale xy; plot "gauss.dat" using 1:2:3 with errorbars'
+    GaussianChain.test(filenames = (
+        "sasfit_gauss2-5-1.5-2-1.dat",
+        "sasfit_gauss2-1-100-1-1.dat")
+    )
 
 # vim: set ts=4 sts=4 sw=4 tw=0:
