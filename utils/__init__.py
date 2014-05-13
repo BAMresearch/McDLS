1383285afb3896a4d5a9fee5b4aa7b2e37ee9817
Ingo Breßler <dev@ingobressler.net>
2014-05-13 15:25:59 +0200

    providing common utility functions via utils.__init__

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # __init__.py
 
-pass
+from cutesnake.utils import (isList, isString, isNonEmptyString, isMap, isSet,
+                             isNumber, isInteger)
+from cutesnake.utils import isLinux, isMac, isWindows, isFrozen
+from cutesnake.utils import testfor, assertName
 
 # vim: set ts=4 sw=4 sts=4 tw=0:
