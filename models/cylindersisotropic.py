eccf0026201d004ea71a5ea5ffef749fb0416563
Ingo Breßler <dev@ingobressler.net>
2014-05-16 16:41:26 +0200

    adjusted CylindersIsotropic for direct parameter value access

--- models/cylindersisotropic.py
+++ models/cylindersisotropic.py
@@ -16,19 +16,19 @@ class CylindersIsotropic(ScatteringModel):
     shortName = "Isotropic Cylinders"
     parameters = (
             FitParameter("radius", 1.0,
-                    displayName = "Cylinder radius",
+                    displayName = "Cylinder Radius",
                     generator = RandomExponential,
                     valueRange = (0.1, numpy.inf), suffix = "nm"),
             FitParameter("length", 10.0,
-                    displayName = "length L of the cylinder",
+                    displayName = "Length L of the Cylinder",
                     generator = RandomExponential,
                     valueRange = (0.1, numpy.inf), suffix = "nm"),
             FitParameter("psiAngle", 0.0,
-                    displayName = "internal parameter -- ignore",
+                    displayName = "Internal Parameter -- ignore",
                     generator = RandomUniform,
                     valueRange = (0.01, 180.01), suffix = "deg."),
             FitParameter("psiAngleDivisions", 303.,
-                    displayName = "orientation integration divisions",
+                    displayName = "Orientation Integration Divisions",
                     valueRange = (1, numpy.inf), suffix = "-"),
             FitParameter("lengthIsAspect", True,
                     displayName = "length value indicates aspect ratio"),
@@ -42,83 +42,54 @@ class CylindersIsotropic(ScatteringModel):
         self.length.setValueRange((1, numpy.inf))
 
     def formfactor(self, dataset, paramValues):
-        # vectorized data and arguments
-        q = dataset.q
-        radius = numpy.array((self.radius(),))
-        length = numpy.array((self.length(),))
-        psiAngleDivisions = numpy.array((self.psiAngleDivisions(),))
-        #unused:
-        psiAngle = numpy.array((self.psiAngle(),))
+        # psi and phi defined in fig. 1, Pauw et al, J. Appl. Cryst. 2010
+        # used in the equation for a cylinder from Pedersen, 1997
 
-        if self.radius.isActive():
-            radius = paramValues[:, 0]
-        if self.length.isActive():
-            idx = int(self.radius.isActive())
-            length = paramValues[:, idx]
-        #remaining parameters are never active fitting parameters
+        dToR = pi/180. #degrees to radian
+        psiRange = self.psiAngle.valueRange()
+        psi = numpy.linspace(psiRange[0], psiRange[1], self.psiAngleDivisions())
 
-        #psi and phi defined in fig. 1, Pauw et al, J. Appl. Cryst. 2010
-        #used in the equation for a cylinder from Pedersen, 1997
-
-        dToR=pi/180. #degrees to radian
-        psiRange=self.psiAngle.valueRange()
-        psi=numpy.linspace(
-                psiRange[0],psiRange[1],psiAngleDivisions)
-
-        fsplit=zeros((len(q),len(psi)))
-        Fcyl=zeros((len(q),len(radius)))
-        for ri in range(len(radius)):
-            radi=radius[ri%len(radius)]
-            if self.lengthIsAspect():
-                halfLength=radi*length[ri%len(length)]
-            else:
-                halfLength=0.5*length[ri%len(length)]
-            qRsina=numpy.outer(q,radi*sin((psi)*dToR))
-            qLcosa=numpy.outer(q,halfLength*cos((psi)*dToR))
-            fsplit=( 2.*scipy.special.j1(qRsina)/qRsina * sinc(qLcosa/pi) )*sqrt(abs(sin((psi)*dToR))[newaxis,:]+0*qRsina)
-            #integrate over orientation
-            Fcyl[:,ri]=numpy.sqrt(numpy.mean(fsplit**2,axis=1)) #should be length q
-
-        return Fcyl
+        if self.lengthIsAspect():
+            halfLength = self.radius() * self.length()
+        else:
+            halfLength = 0.5 * self.length()
+        qRsina = numpy.outer(dataset.q, self.radius() * sin((psi) * dToR))
+        qLcosa = numpy.outer(dataset.q, halfLength * cos((psi) * dToR))
+        fsplit = ((2.*scipy.special.j1(qRsina)/qRsina * sinc(qLcosa/pi))
+                  * sqrt(abs(sin((psi) * dToR))[newaxis,:] + 0. * qRsina))
+        #integrate over orientation
+        return numpy.sqrt(numpy.mean(fsplit**2, axis=1)) # should be length q
 
     def volume(self, paramValues):
-        idx = 0
-        radius=paramValues[:,0]
-        if self.length.isActive():
-            idx+=1
-            length =paramValues[:,idx]
-        else:
-            length=self.length()
         if self.lengthIsAspect():
-            halfLength=radius*length
+            halfLength = self.radius() * self.length()
         else:
-            halfLength=length/2.
-
-        v = pi*radius**2*(halfLength*2)
+            halfLength = self.length() / 2.
+        v = pi * self.radius()**2 * (halfLength * 2.)
         return v**self.compensationExponent
 
 CylindersIsotropic.factory()
 
-if __name__ == "__main__":
-    from cutesnake.datafile import PDHFile, AsciiFile
-    # FIXME: use SASData.load() instead
-    pf = PDHFile("sasfit_gauss2-1-100-1-1.dat")
-    model = CylindersIsotropic()
-    model.radius.setValue(1.)
-    model.radius.setActive(False)
-    model.length.setValue(100.)
-    model.length.setActive(False)
-    model.psiAngle.setValue(1.)
-    model.psiAngle.setActive(False)
-    model.psiAngleDivisions.setValue(303)
-    model.psiAngleDivisions.setActive(False)
-    intensity = (model.formfactor(pf.data, None).reshape(-1))**2
-    q = pf.data[:, 0]
-    oldInt = pf.data[:, 1]
-    delta = abs(oldInt - intensity)
-    result = numpy.dstack((q, intensity, delta))[0]
-    AsciiFile.writeFile("CylindersIsotropic.dat", result)
-    # call it like this:
-    # PYTHONPATH=..:../mcsas/ python brianpauwgui/gaussianchain.py && gnuplot -p -e 'set logscale xy; plot "gauss.dat" using 1:2:3 with errorbars'
+#if __name__ == "__main__":
+#    from cutesnake.datafile import PDHFile, AsciiFile
+#    # FIXME: use SASData.load() instead
+#    pf = PDHFile("sasfit_gauss2-1-100-1-1.dat")
+#    model = CylindersIsotropic()
+#    model.radius.setValue(1.)
+#    model.radius.setActive(False)
+#    model.length.setValue(100.)
+#    model.length.setActive(False)
+#    model.psiAngle.setValue(1.)
+#    model.psiAngle.setActive(False)
+#    model.psiAngleDivisions.setValue(303)
+#    model.psiAngleDivisions.setActive(False)
+#    intensity = (model.formfactor(pf.data, None).reshape(-1))**2
+#    q = pf.data[:, 0]
+#    oldInt = pf.data[:, 1]
+#    delta = abs(oldInt - intensity)
+#    result = numpy.dstack((q, intensity, delta))[0]
+#    AsciiFile.writeFile("CylindersIsotropic.dat", result)
+#    # call it like this:
+#    # PYTHONPATH=..:../mcsas/ python brianpauwgui/gaussianchain.py && gnuplot -p -e 'set logscale xy; plot "gauss.dat" using 1:2:3 with errorbars'
 
 # vim: set ts=4 sts=4 sw=4 tw=0:
