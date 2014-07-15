ec0147fd7b2b827496686e32ff1c25c9e15ed8a1
Ingo Breßler <dev@ingobressler.net>
2014-07-16 01:06:13 +0200

    utils.processEventLoop() made available

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,5 +6,6 @@ from cutesnake.utils import (isList, isString, isNonEmptyString, isMap, isSet,
 from cutesnake.utils import isLinux, isMac, isWindows, isFrozen
 from cutesnake.utils import testfor, assertName
 from cutesnake.utils import mixedmethod
+from cutesnake.utilsgui import processEventLoop
 
 # vim: set ts=4 sw=4 sts=4 tw=0:
