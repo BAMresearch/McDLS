1c281be92a1a5a9c0d6dd3d25fbffdc002195ab9
Brian <brian@stack.nl>
2014-08-18 21:57:21 +0900

    Additions to defaults in sasunit, including a "none" -unit for unitless parameters.

--- sasunit.py
+++ sasunit.py
@@ -43,6 +43,22 @@ class SASUnit(object):
                 1e-3 : u"mm",
                 1e-2 : u"cm",
                 1e0 : u"m"},
+            'area' : { 1e-20 : u"Å²",
+                1e-18 : u"nm²",
+                1e-12 : u"µm²",
+                 1e-6 : u"mm²",
+                 1.   : u"m²"}
+            'volume' : { 1e-30 : u"Å³",
+                1e-27 : u"nm³",
+                1e-18 : u"µm³",
+                 1e-9 : u"mm³",
+                   1. : u"m³"}
+            'SLD' : { 1e20 : u"Å⁻²",
+                1e18 : u"nm⁻²",
+                1e12 : u"µm⁻²",
+                 1e6 : u"mm⁻²",
+                 1e4 : u"cm⁻²",
+                  1. : u"m⁻²"}
             'q' :  { 1e10 : u"Å⁻¹",
                 1e9 : u"nm⁻¹",
                 1e6 : u"µm⁻¹",
@@ -51,6 +67,9 @@ class SASUnit(object):
                 1e0 : u"m⁻¹"},
             'I' :  { 1e2 : u"(cm sr)⁻¹",
                 1e0 : u"(m sr)⁻¹"},
+            'none' : { 1. : u"",
+                1. : u"-"}
+
             }
 
     def __init__(self, **kwargs):
