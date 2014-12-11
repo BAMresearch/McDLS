ec9856eab608a1340049c1b25ab699c17ff8ad11
Ingo Breßler <dev@ingobressler.net>
2014-12-11 20:01:20 +0100

    debugging the SciEntryBox and its number validator

--- gui/scientrybox.py
+++ gui/scientrybox.py
@@ -3,12 +3,25 @@
 
 from QtGui import QLineEdit, QDoubleValidator
 from QtCore import Qt
+import sys
+
+class MyValidator(QDoubleValidator):
+    def validate(self, input, pos):
+        result = QDoubleValidator.validate(self, input, pos)
+        print >>sys.__stderr__, "validate '{}'".format(input), pos, result
+        print >>sys.__stderr__, "validator:", self.bottom(), self.top(), self.decimals(), self.notation()
+        return result
+
+    def fixup(self, input):
+        result = QDoubleValidator.fixup(self, input)
+        print >>sys.__stderr__, "fixup '{}' -> '{}'".format(input, result)
+        return result
 
 class SciEntryBox(QLineEdit):
     def __init__(self, parent):
         QLineEdit.__init__(self, parent)
 
-        lval = QDoubleValidator()
+        lval = MyValidator()
         lval.setNotation(QDoubleValidator.ScientificNotation)
         lval.setRange(-1e100, 1e100)
         lval.setDecimals(9)
@@ -21,6 +34,10 @@ class SciEntryBox(QLineEdit):
         setattr(self, "maximum", lval.top)
         setattr(self, "setDecimals", lval.setDecimals)
         setattr(self, "decimals", lval.decimals)
+
+    def focusOutEvent(self, event):
+        print >>sys.__stderr__, "focusOutEvent, loosing focus"
+        QLineEdit.focusOutEvent(self, event)
 
     def setRange(self, lo, hi):
         self.setMinimum(lo)
