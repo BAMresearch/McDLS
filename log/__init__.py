0ce6f8081f44d875a1d836281d8ba3690122ac98
Ingo Breßler <dev@ingobressler.net>
2015-01-03 23:50:22 +0100

    log.getWidgetHandlers() returns active log widget sinks

--- log/__init__.py
+++ log/__init__.py
@@ -8,4 +8,8 @@ from widgethandler import WidgetHandler
 
 log = logging.getLogger()
 
+def getWidgetHandlers():
+    """Returns all active WidgetHandlers for logging."""
+    return list((h for h in log.handlers if isinstance(h, WidgetHandler)))
+
 # vim: set ts=4 sw=4 sts=4 tw=0:
