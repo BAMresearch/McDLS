41f80ad88a4472c99f010294c589d48370d76412
Brian <brian@stack.nl>
2015-11-11 07:56:30 +0100

    resolve commit conflict...

--- models/scatteringmodel.py
+++ models/scatteringmodel.py
@@ -217,25 +217,15 @@ class SASModel(ScatteringModel):
         v = self.vol(compensationExponent = compensationExponent,
                      useSLD = useSLD)
 
-<<<<<<< HEAD
-        if data.doSmear:
-            # locs contains the two-dimensional q-value array for integration. 
-=======
         if data.config.smearing is not None:
->>>>>>> calculation of smearing data works, but fails to be available at model.calcIntensity [WIP]
             kansas = data.locs.shape
             # the ff functions might only accept one-dimensional q arrays
             locs = data.locs.reshape((data.locs.size))
             ff = self.ff(locs).reshape(kansas)
-<<<<<<< HEAD
-            # we integrate the smeared result in one direction. This
-            # implementation is for slit-smeared Kratky-type instruments only.
-=======
             dU, weightFunc = data.config.smearing.prepared
             import sys
             print >>sys.__stderr__, "prepared", dU, weightFunc
             print >>sys.__stderr__, unicode(data.config.smearing)
->>>>>>> calculation of smearing data works, but fails to be available at model.calcIntensity [WIP]
             it = 2 * np.trapz(ff**2 * v**2 * # outer() ?
                     (0 * ff + weightFunc), x = dU, axis = 1) 
         else:
