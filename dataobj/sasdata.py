dfaa1863b86f3f7617a795dccf4217688b7ab116
Ingo Breßler <dev@ingobressler.net>
2015-11-16 16:49:30 +0100

    SASData: replace print by logging.info() to workaround encoding issues associated with python2 print()

--- dataobj/sasdata.py
+++ dataobj/sasdata.py
@@ -333,7 +333,7 @@ class SASData(DataObj):
                 unit = self.ii.unit, editable = True)
         self.qi.limit = [self.qi.value.min(), self.qi.value.max()]
         print [self.qi.value.min(), self.qi.value.max()]
-        print self.qi.limsString
+        logging.info(self.qi.limsString)
         if rawArray.shape[1] > 3: # psi column is present
             self.pi = DataVector(u'ψ', rawArray[:, 3], unit = Angle(u"°"))
         else:
