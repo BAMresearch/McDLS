2b85575cdcaac756276cb3497512b21791d9c424
Ingo Breßler <dev@ingobressler.net>
2015-12-03 15:50:34 +0100

    passing the unit of tau from DLSData -> DLSConfig

--- dataobj/dlsdata.py
+++ dataobj/dlsdata.py
@@ -339,6 +339,8 @@ class DLSData(DataObj):
     def setConfig(self, config):
         if not super(DLSData, self).setConfig(config):
             return # no update, nothing todo
+        self.config.x0Low.setUnit(self.tau.unit)
+        self.config.x0High.setUnit(self.tau.unit)
         self.config.register("x0limits", self.x0.setLimit)
         self.x0.limit = self.config.x0Low(), self.config.x0High()
 
