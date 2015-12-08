886120bfb270e98456dbe7b61bc1ea63042ae731
Ingo Breßler <dev@ingobressler.net>
2015-12-08 22:26:40 +0100

    improved DBG function for development

--- utils/devtools.py
+++ utils/devtools.py
@@ -6,9 +6,22 @@ Definitions used during development only.
 This module is supposed to not being required in a release package.
 """
 
+from __future__ import print_function
 import sys
+import inspect
+import os.path
 
 def DBG(*args):
-    print >>sys.__stderr__, " ".join([unicode(a) for a in args])
+    stack = inspect.stack()
+    prefix = ""
+    if len(stack) > 1:
+        frame = stack[1][0]
+        head, fn = os.path.split(frame.f_code.co_filename)
+        head, mod = os.path.split(head)
+        fn = os.path.splitext(fn)[0]
+        func = inspect.getframeinfo(frame).function
+        prefix = u"[{0:04d}|{1}]".format(frame.f_lineno, '.'.join((mod, fn)))
+    print(u" ".join([prefix, func]
+        + [unicode(a) for a in args]), file = sys.__stderr__)
 
 # vim: set ts=4 sts=4 sw=4 tw=0:
