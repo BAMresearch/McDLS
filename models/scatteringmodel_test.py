dc221f5a212c319c25fa625e41728d9753d3cb78
Ingo Breßler <dev@ingobressler.net>
2015-12-09 14:15:26 +0100

    simple serialization test for ScatteringModel

---
+++ models/scatteringmodel_test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# models/scatteringmodel_test.py
+
+from models.scatteringmodel import ScatteringModel
+from utils.parameter import FitParameter
+
+class DummyModel(ScatteringModel):
+    shortName = "DummyModelName"
+    parameters = (
+        FitParameter(name = "testp0", value = 3.4, valueRange = (.5, 6.7)),
+        FitParameter(name = "testp1", value = 4, valueRange = (1, 5))
+    )
+    def calcIntensity(*args): pass
+    def volume(*args): pass
+    def formfactor(*args): pass
+
+# TODO: does not work for active parameters, see McSAS.plot()
+#       -> serialization support for histograms/fitparameters has to be added
+def testSerialization():
+    import pickle
+    a = DummyModel.factory()()
+    data = pickle.dumps(a)
+    a2 = pickle.loads(data)
+    print a
+    print a2
+    assert a == a2
+
+# vim: set ts=4 sts=4 sw=4 tw=0:
