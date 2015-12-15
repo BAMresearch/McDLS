b5084e8436cc4b659a9e5838a104e5ac4175894e
Brian <brian@stack.nl>
2015-12-15 16:30:35 +0100

    Only call spherical size estimator when it exists.

--- gui/filelist.py
+++ gui/filelist.py
@@ -50,9 +50,10 @@ class FileList(DataList):
                           processSourceFunc = loaddataobj)
 
     def itemDoubleClicked(self, item, column):
-        valueRange = item.data().sphericalSizeEst()
-        self.sigSphericalSizeRange.emit(min(valueRange), max(valueRange))
-        # self.sigShannonChannels.emit(item.data().shannonChannelEst()
+        if hasattr(item.data(), "sphericalSizeEst"):
+            valueRange = item.data().sphericalSizeEst()
+            self.sigSphericalSizeRange.emit(min(valueRange), max(valueRange))
+            # self.sigShannonChannels.emit(item.data().shannonChannelEst()
 
     def setupUi(self):
         self.listWidget.setAlternatingRowColors(True)
