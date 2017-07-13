658172a8c7a8820341aa13c83190b1299d8b847c
Ingo Breßler <dev@ingobressler.net>
2017-07-13 18:32:54 +0200

    en-/disableFormatter() in log module
    - allows to temporarily remove/add leading timestamps to log messages

--- log/__init__.py
+++ log/__init__.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import
 import logging
-from .log import (timestampFormat, timestamp, timestampFormatted,
+from .log import (timestampFormat, timestamp, timestampFormatted, formatter,
                   replaceStdOutErr, replaceHandler, addHandler, removeHandler)
 from .widgethandler import WidgetHandler
 
@@ -12,5 +12,13 @@ log = logging.getLogger()
 def getWidgetHandlers():
     """Returns all active WidgetHandlers for logging."""
     return list((h for h in log.handlers if isinstance(h, WidgetHandler)))
+
+def disableFormatter():
+    for h in log.handlers:
+        h.setFormatter(logging.Formatter())
+
+def enableFormatter():
+    for h in log.handlers:
+        h.setFormatter(formatter())
 
 # vim: set ts=4 sw=4 sts=4 tw=0:
